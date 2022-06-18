c736f5ea1ed07b82f0e01e47e115ab8c75fe647a
Hakim Jurimi <77281311+hkmjrmi@users.noreply.github.com>
2022-06-19 03:48:31 +0800

    Update navbar and forms list

--- resources/views/dashboard/lecturer/forms.blade.php
+++ resources/views/dashboard/lecturer/forms.blade.php
@@ -10,11 +10,14 @@
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" width="100%" cellspacing="0">
+                    <thead>
                     <tr>
                         <th>No</th>
                         <th>Name</th>
                         <th>Action</th>
                     </tr>
+                    </thead>
+                    <tbody>
                     <tr>
                         <td>1</td>
                         <td>F1</td>
@@ -22,25 +25,25 @@
                     </tr>
                     <tr>
                         <td>2</td>
-                        <td>F2</td>
+                        <td>F2 - Project Motivation Evaluation Form</td>
                         <td><a href="forms/f2">View</td>
                     </tr>
                     <tr>
                         <td>3</td>
-                        <td>F3</td>
+                        <td>F3 - Literature Review Evaluation Form</td>
                         <td><a href="forms/f3">View</td>
                     </tr>
                     <tr>
                         <td>4</td>
-                        <td>F4</td>
+                        <td>F4 - Methodology Evalution Form </td>
                         <td><a href="forms/f4">View</td>
                     </tr>
                     <tr>
                         <td>5</td>
-                        <td>F13</td>
+                        <td>F13 - Lean Model Canvas</td>
                         <td><a href="forms/f13">View</td>
                     </tr>
-
+                    </tbody>
                 </table>
             </div>
         </div>
